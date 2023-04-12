c335da04887bf52c1689dddfb9bb1310c78787ed
David <david@gnyrftacode.se>
2023-04-12 19:58:23 +0200

    fixed HSV to RGB conversion

--- gameboy.ino
+++ gameboy.ino
@@ -60,6 +60,7 @@ int command = COMMAND_NO;
   // put your setup code here, to run once:
 
    bool tryLoop{false};
+   bool tryLoop2{false};
 
 uint16_t changeThisLater[3];
 
@@ -93,19 +94,36 @@ void loop() //Presents the menu and if 'Mandelbrot' is clicked it starts drawing
 
   if (button.isPressed()) {
     Serial.println("The button is pressed");//If the button is pressed on the 'mandelbrot' text, launch the mandelbrot program. 
-    if(30>cursorY>10){
+    Serial.print(cursorY);
+    if(30>cursorY>10)
+    {
     Paint_DrawString_EN(30, 10, "Mandelbrot", &Font24, BLUE, WHITE);//Show that the button has been pressed. 
     delay(200);
+    } 
+    else if(cursorY>30)
+    {
+      tryLoop2=true;
+      Serial.println("tryLoop2 set to true");
     }
   }
-
   if (button.isReleased()) {
     Serial.println("The button is released");
+    if(30>cursorY>10)
+    {
     Paint_DrawString_EN(30, 10, "Mandelbrot", &Font24, WHITE, BLACK);
     tryLoop=true; //Launch Mandelbrot program. 
+    }
+    else if(60>cursorY>30)
+    {
+      tryLoop2=true;
+      Serial.println("tryLoop2 set to true");
+    }
+
   }
  // Paint_DrawRectangle(cursorXOld, cursorYOld, 17, 17, WHITE,  DOT_PIXEL_2X2,DRAW_FILL_FULL);
   Paint_DrawString_EN(30, 10, "Mandelbrot", &Font24, WHITE, BLACK);
+  Paint_DrawString_EN(30,30, "Gradient", &Font24, WHITE, BLACK);
+
   if(!(cursorX==cursorXOld && cursorY==cursorYOld))
   {
   Paint_DrawString_EN(cursorXOld, cursorYOld, "+", &Font24, WHITE, WHITE);//Take away this and you have an etch-a-sketch. : ) 
@@ -118,6 +136,10 @@ void loop() //Presents the menu and if 'Mandelbrot' is clicked it starts drawing
   nextOriginX=tempNextOriginX;
   nextOriginY=tempNextOriginY;
   scale+=1.5;
+  }
+  if(tryLoop2)
+  {
+    HSVCycle();
   }
 }
 
@@ -209,7 +231,7 @@ void changeCursorValue()
     Serial.println("COMMAND LEFT");
     if(cursorX > 0 )
     {
-    cursorX--;
+    cursorX-=3;
     }
   }
 
@@ -217,7 +239,7 @@ void changeCursorValue()
     Serial.println("COMMAND RIGHT");
     if(cursorX < 240)
     {
-    cursorX++;
+    cursorX+=3;
     }
   }
 
@@ -225,7 +247,7 @@ void changeCursorValue()
     Serial.println("COMMAND UP");
     if(cursorY > 0)
     {https://www.rapidtables.com/convert/color/hsv-to-rgb.html
-    cursorY--; 
+    cursorY-=3; 
     }   
   }
 
@@ -233,54 +255,85 @@ void changeCursorValue()
     Serial.println("COMMAND DOWN");
     if(cursorY < 320)
     {
-    cursorY++;
+    cursorY+=3;
     }
   }
 }
+ 
  void convertHSVtoRGB(int H)
   {
-    double X = 1-fabs(fmod(H/60,2)-1);
+    double h = (double)H;
+    double s = h/60;
+    //Serial.println(s);
+    double X = 1-fabs(fmod(s,2)-1);
+    //Serial.println(X);
     H = fmod(H,360);
-    
-    if(0<=H<60)
+    uint16_t x = (int)(X*255);
+    Serial.println(x);
+    if(H<60)
     {
-      changeThisLater[0]= 1;
-      changeThisLater[1]=X;
+      changeThisLater[0]= 255;
+      changeThisLater[1]=x;
       changeThisLater[2]=0;
     }
-    else if(60<=H<120)
+    else if((60<=H)&&(H<120))
     {
-      changeThisLater[0]=X;
-      changeThisLater[1]=1;
+      changeThisLater[0]=x;
+      changeThisLater[1]=255;
       changeThisLater[2]=0;
     }
-    else if(120<=H<180)
+    else if((120<=H)&&(H<180))
     {
       changeThisLater[0]=0;
-      changeThisLater[1]=1;
-      changeThisLater[2]=X;
+      changeThisLater[1]=255;
+      changeThisLater[2]=x;
     }
-    else if(180<=H<240)
+    else if((180<=H)&&(H<240))
     {
       changeThisLater[0]=0;
-      changeThisLater[1]=X;
-      changeThisLater[2]=1;
+      changeThisLater[1]=x;
+      changeThisLater[2]=255;
     }
-    else if(240<=H<300)
+    else if((240<=H)&&(H<300))
     {
-      changeThisLater[0]=X;
+      changeThisLater[0]=x;
       changeThisLater[1]=0;
-      changeThisLater[2]=1;
+      changeThisLater[2]=255;
     }
-    else if(300<=H<360)
+    else if((300<=H)&&(H<360))
     {
-      changeThisLater[0]=1;
+      changeThisLater[0]=255;
       changeThisLater[1]=0;
-      changeThisLater[2]=X;
+      changeThisLater[2]=x;
     }
-    changeThisLater[0]=changeThisLater[0]*255;
-    changeThisLater[1]=changeThisLater[1]*255;
-    changeThisLater[2]=changeThisLater[2]*255;
+    Serial.print(changeThisLater[0]);
+    Serial.print(changeThisLater[1]);
+    Serial.print(changeThisLater[2]);
   }
+  
 
+
+
+ 
+void HSVCycle()
+{
+	int H = 0;
+	while(true)
+		{
+			for(uint16_t x = 0; x<240;x++)
+				{
+					for(uint16_t y=0;y<320;y++)
+						{
+							double xd{x};
+							double yd{y};
+							Serial.print("should be drawing colours");
+							convertHSVtoRGB(H);
+							Paint_SetPixel(x,y,convertToUWORD(changeThisLater[0],changeThisLater[1],changeThisLater[2]));//This function works and 'convertToUWORD' works.
+
+						}
+          H++;
+				}
+
+		}
+}
 #endif
